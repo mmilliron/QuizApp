dae1303db2c2f6a5f86022cb310ffb16bf402535
mmilliron <32225206+mmilliron@users.noreply.github.com>
2019-04-19 12:49:10 -0400

    Add files via upload

---
+++ datastore.js
@@ -0,0 +1,52 @@
+const STORE = [
+    {
+      question: 'Why is prescribed burning important?',
+      answers: [
+        'It kills all the nasty bugs.',
+        'It reduces wildfire risk, improves wildlife habitat and promotes healthy ecosystems.',
+        'Smokey Bear wants the forest to always be black.',
+        'Burning helps convicted arsonist make a paycheck'
+        ],
+      correctAnswer: 'It reduces wildfire risk, improves wildlife habitat and promotes healthy ecosystems.',
+    },
+    {
+      question: 'What state leads the nation in prescribed burning',
+      answers: [
+        'Alabama',
+        'California',
+        'Florida',
+        'Alaska'
+        ],
+        correctAnswer: 'Florida',
+    },
+    {
+      question: 'What is the national annual prescribed burn record by a government aganecy?',
+      answers: [
+        '385 acres',
+        '246,104 acres',
+        '101,202 acres',
+        '1,000,000,000 acres'
+        ],
+        correctAnswer: '246,104 acres',
+    },
+    {
+      question: 'What species is dependent on prescribed fire to survive?',
+      answers: [
+        'Flatwoods Salamander',
+        'Wild Turkey',
+        'Bob-White Quail',
+        'All of the above'
+        ],
+      correctAnswer: 'All of the above',
+    },
+    {
+      question: 'How can prescribed burners promote a positive public preception of prescibed burning',
+      answers: [
+        'Ensure all burns remain controlled and provide for public saftey.',
+        'Burn all the pretty trees down.',
+        'Burn all those pesky houses down.',
+        'BBQ wildlife in a uncontrolled blaze from Hell.'
+        ],
+      correctAnswer: 'Ensure all burns remain controlled and provide for public saftey.',
+    },
+];
